fd2a71725bc29c2519e597421aea0251f0e7c8a5
apeura <anni.peura@tuni.fi>
2023-04-14 13:55:35 +0300

    cleaning up utility functions and updating backend

--- frontend/util/utility.py
+++ frontend/util/utility.py
@@ -16,22 +16,6 @@ def format_score():
 
     for score in all_scores:
         print(score["id"], score["time"], score["name"])
-        id = score["id"]
-        time = score["time"]
-        name = score["name"]
-        all_scores_string += f'{id} {time} {name} "\n"'
-
-    #player_lines = all_data.split('\n')
-    #for data in player_lines:
-    #    values = data.split(',')
-        
-    #    one_player_score = {'id': int(values[0]), 'time': (values[1]), 'name': (values[2])}
-    #    all_scores_dict.append(one_player_score)
-
-     #   del values[0]#
-     #   values_string = " ".join(values)
-     #   values_string += '\n'
-      #  all_scores += values_string
 
     #print(all_scores_dict)
     DS_list = read_score()
@@ -55,44 +39,22 @@ def read_score():
 
 # Saves data to the text file as "(id),(time),(name)"
 def save_to_score(id, time, name):
-    with open('scores.json', 'r') as file:
-        scores_data = json.load(file)
-        new_score = {"id": id, "time": time, "name": name}
+
+    scores_data = json.load(scores.json)
+    new_score = {"id": id, "time": time, "name": name}
+    
+    with open('scores.json', 'a') as file:
         scores_data["scores"].append(new_score)
-
+        print("done!")
+        #with open("scores.json", "w") as file:
+        #    json.dump(scores_data, file, indent=2)
     
-        with open("scores.json", "w") as file:
-            json.dump(scores_data, file, indent=2)
-    
-    #f.write("\n" + f"(id: {id}, time: {time}, name: {name})")
-    #f.close()
-
-# Creates a list of all current ID values
-def get_id_list():
-    id_all = []
-    all_data = read_score()
-    split_data = all_data.split('\n')
-    for data in split_data:
-        values = data.split(',')
-        player_id = {'id': int(values[0])}
-        id_all.append(player_id)
-
-    return id_all
 
 # Generates
 def generate_id():
     # Counts the amount of lines in the text file
     # so that the value can be used for the ID generation.
-    with open('scores.json') as f:
-        id_line_amount = sum(1 for _ in f)
-    
-    new_id = id_line_amount + 1
+    scores_dict = json.loads(read_score())
+    new_id = len(scores_dict['scores']) + 1
 
     return new_id
-
-    
-def main():
-    format_score()
-
-if __name__ == "__main__":
-    main()
