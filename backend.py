ef2f86df194a8a8d16d55c3dc62a0eb7f1e07db7
apeura <anni.peura@tuni.fi>
2023-04-26 21:17:29 +0300

    trying different saving set up for data

--- backend.py
+++ backend.py
@@ -152,7 +152,7 @@ def add_highscore():
 
     new_data = request.json
 
-    all_data = {'scores': old_data['scores'] + new_data['scores']}
+    all_data = {"scores": old_data["scores"] + new_data["scores"]}
     print(all_data)
 
 
