58621f58094333df6d9cc4d54cd9a8e6f112b68c
apeura <anni.peura@tuni.fi>
2023-04-26 15:38:00 +0300

    save troubleshooting, manual jsonification of data

--- frontend/util/utility.py
+++ frontend/util/utility.py
@@ -52,6 +52,8 @@ def save_to_score(user_data):
     with open(scores_path) as f:
         data = json.load(f)
 
+
+
     data["scores"].append(user_data)
     #{'id': 6, 'time': '00:00:01', 'name': 'MORROJR'}
     #print("USER_DATA IN SAVE_TO_SCORE: ", user_data) 
@@ -60,9 +62,19 @@ def save_to_score(user_data):
 
    #data_json_format = json.dumps(data)
     print("DATA IS:", data)
+    # {'scores': [{'id': 1, 'time': '00:00:01', 'name': 'Leevi'}, {'id': 2, 'time': '00:33:00', 'name': 'Hanna'}, {'id': 3, 'time': '00:22:00', 'name': 'Viivi'}, {'id': 4, 'time': '00:00:02', 'name': 'ANNI'}, {'id': 5, 'time': '00:00:01', 'name': 'Lasse'}, {'id': 6, 'time': '00:00:03', 'name': 'HASSUA'}]}
     
+    json_format_data: {"scores":[]}
+
+    for score in data:
+        id = score['id']
+        time = score['time'] 
+        name = score['name']
+        single_score = {"id": {id}, "time": "{time}", "name": "{name}"}
+        json_format_data["scores"].append(single_score)
+
     with open(scores_path, 'w') as f:
-        json.dump(data, f, ensure_ascii=False)
+        json.dump(json_format_data, f, ensure_ascii=False)
 
     print("saved to json!")
 
