7b7e45beba9faa41433e2a053481a9f66ff0230b
apeura <anni.peura@tuni.fi>
2023-04-27 21:40:21 +0300

    scores.json related utility functions moved to backend

--- frontend/util/utility.py
+++ frontend/util/utility.py
@@ -2,67 +2,11 @@
 import json
 from datetime import datetime
 import os
-from ...backend import read_score
 
 #fix to scores.json not being found, determined path
 scores_path = os.path.join(os.path.dirname(__file__), '..', '..', 'scores.json')
 
 url = "https://hangman-highscores-amif.onrender.com/scores"
-
-#returns data in asc order (default)
-def sort_score(descending=False):
-    all_data = read_score()
-    all_scores = all_data["scores"]
-    times = sorted(all_scores, key=lambda k: k["time"], reverse=descending)
-    sorted_scores = []
-
-    for score in times:
-        id = score["id"]
-        time = score["time"] 
-        name = score["name"]
-        single_score = {"id": id, "time": str(time), "name": str(name)}
-        sorted_scores.append(single_score)
-
-    return sorted_scores
-
-#Returns the scores as a list, excluding the id (id is not needed)
-#So that the scores can be shown in the html page
-def make_2D_array(descending=False):
-    all_data = read_score()
-    all_scores = all_data["scores"]
-
-    times = sorted(all_scores, key=lambda k: k["time"], reverse=descending)
-    score_list = []
-
-    for score in times:
-        time = score["time"] 
-        name = score["name"]
-        single_score = [str(time), str(name)]
-        score_list.append(single_score)
-
-    return score_list
-
-# Checks if new score should be added to top 50
-def score_is_added_to_top50(new_score):
-
-    new_time = new_score["time"]
-    #print("SCORE_IS_ADDED new_time is", new_time)
-
-    with open(scores_path) as f:
-        user_data = json.load(f)
-        #print("SCORE_IS_ADDED... USER_DATA", user_data)
-    f.close()
-
-    if len(user_data["scores"]) < 50:
-        return True
-
-    # Sort the scores by time
-    sorted_scores = sorted(user_data["scores"], key=lambda x: x["time"])
-
-    if new_time < sorted_scores[-1]["time"]:
-        return True
-
-    return False
 
 #fills any gaps when a score has been deleted by fixing existing ids
 def adjust_ids(dict, removed_id):
