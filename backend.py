6914012fdc65bd56f5a02f6f5b82292ba1b3a69a
apeura <anni.peura@tuni.fi>
2023-04-27 11:07:02 +0300

    adding firebase fix for save issue

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+
 from flask import render_template, Flask, jsonify, abort, make_response, request, json
 from frontend.util.utility import *
 from dotenv import load_dotenv
@@ -9,6 +10,55 @@ app = Flask(__name__)
 load_dotenv()
 
 API_KEY = os.environ.get('API_KEY')
+
+####################### FIX FOR SAVING START
+####################### FIX FOR SAVING
+
+import os
+from flask import Flask, request, jsonify, json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+import tempfile
+
+# luetaan firebase ympäristömuuttuja
+# tee render.comiin uusi muuttuja nimeltä firebase jonka
+# sisältö on json tiedosto jonka saat firebaselta
+json_str = os.environ.get('firebase')
+
+# tallennetaan ympäristömuuttujan sisältö väliaikaiseen tiedostoon
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    f.write(json_str)
+    temp_path = f.name
+
+# luetaan tiedostosta json filu
+cred = credentials.Certificate(temp_path)
+
+# tee render.comiin ympäristömuuttuja bucket, jonka sisältö
+# esim: mydatabase-38cf0.appspot.com
+firebase_admin.initialize_app(cred, {
+    'storageBucket': os.environ.get('bucket')
+})
+bucket = storage.bucket()
+
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    # Load the contents of the highscores file from Firebase Storage
+    blob = bucket.blob('highscores.json')
+    content = blob.download_as_string().decode('utf-8')
+    data = json.loads(content)
+    return jsonify(data)
+
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    # Load the customer data from the request
+    customer = request.get_json()
+    customer_json = json.dumps(customer)
+    # Upload the updated highscores file to Firebase Storage
+    blob = bucket.blob('highscores.json')
+    blob.upload_from_string(customer_json, content_type='text/plain')
+    return jsonify({'message': 'Customer added successfully!'})
+
 
 # this method is used to check the validity of the password
 # sent with requests to the backend
@@ -22,6 +72,9 @@ def check_api_key(pw):
     result = bcrypt.checkpw(pw.encode('utf-8'), hash)
     # returned values are boolean type
     return True if result else False
+
+####################### FIX FOR SAVING
+####################### FIX FOR SAVING END
 
 scores_str = read_score()
 
--- test/test_hangman.py
+++ test/test_hangman.py
@@ -1,9 +1,28 @@
 # python3 -m unittest test.test_hangman
+#E
+#======================================================================
+#ERROR: test_hangman (unittest.loader._FailedTest)
+#----------------------------------------------------------------------
+#ImportError: Failed to import test module: test_hangman
+#Traceback (most recent call last):
+#  File "/usr/lib/python3.10/unittest/loader.py", line 154, in loadTestsFromName
+#    module = __import__(module_name)
+#  File "/mnt/c/Users/peura/project-work-hangman/test/test_hangman.py", line 5, in <module>
+#    from frontend.hangman import execute_menu_choice
+#  File "/mnt/c/Users/peura/project-work-hangman/frontend/hangman.py", line 5, in <module>
+#    from util.drawings import draw_hangman
+#ModuleNotFoundError: No module named 'util'
+
+import os
+#print(os.getcwd()) #/mnt/c/Users/peura/project-work-hangman/test
+
+import sys
+#sys.path.append("/mnt/c/Users/peura/project-work-hangman/test")
 
 import unittest
 
 from frontend.hangman import execute_menu_choice
-from frontend.util.drawings import *
+from frontend.util.drawings import draw_hangman
 from frontend.util.utility import *
 
 class TestValidation(unittest.TestCase):
